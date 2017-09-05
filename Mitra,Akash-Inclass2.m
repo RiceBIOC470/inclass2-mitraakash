8ab43db9c1524b21f39c1ce351ab0eb292dfbc96
Mitra <amitra2@docmm0cl5001470.local>
2017-09-05 08:44:37 -0500

    First commit

--- Mitra,Akash-Inclass2.m
+++ Mitra,Akash-Inclass2.m
@@ -1,17 +1,31 @@
+
+%Akash Mitra
+%am132
 %% Inclass assignment 2
 
 % 1. a. fill in this loop with a conditional statement so that it displays
 % only the odd numbers
 
 for ii = 1:20
+    if(rem(ii,2) == 1)
+        disp(ii)
+    else
+    end
+    
 end
 
 % b. Write a new loop to display the same odd numbers which doesn't use a conditional statement 
+for jj = 0:1:20
+    disp(jj)
+end 
+
+
 
 % c. display the same odd numbers without a loop or conditional (hint use
 %  num2str)
 
-
+kk = num2str(1:2:20)
+disp(kk)
 %% 2. Vectorize the following code (that is produce the same result in z
 %without using a loop). 
 
@@ -21,35 +35,62 @@ for ii = 1:100
     z(ii) = ii*ii;
 end
 
+%Answer
+ii = 1:100
+sumofsqr = ii.^2
+
 %b. 
 z = 0;
 for ii = 1:100
     z = z + ii;
 end
 
+%Answer
+v = cumsum(1:100)
+disp(v(end))
+
+
 %c.
 xx = rand(50,2);
 
 for ii = 1:50
-    z(ii) = sqrt(xx(ii,1)^2+xx(ii,x)^2);
+    z(ii) = sqrt(xx(ii,1)^2+xx(ii,2)^2);
 end
 
+%Answer
+%dist = sqrt(sum((xx,1).^2+(xx,2).^2))
+
+
 % d. what is the formula in c computing?
+% Distance
+
 
 %% plotting
 
 %a. make a plot of sin(x) and cos(x) vs x from x = 0 to 4*pi on the same axis where the graph
 %of sin(x) vs x is a red-dashed line and the graph of cos(x) vs x is a
 %blue solid line.
+a = 0:pi/100:4*pi;
+b = sin(a);
+c = cos(a);
+plot(a,b, 'r--',a, c, 'b-')
 
 %b. repeat the above but where each graph is in a different subplot in the
 % same figure. 
+a = 0:pi/100:4*pi;
+b = sin(a);
+c = cos(a);
+subplot(2,1,1); plot(b,  'r--')
+subplot(2,1,2); plot(c,  'b-')
+
 
 %c. here is some data, imagine they are x and y coordinates for cells. 
 xy_dat = rand(100,2);
 %plot the data in the xy plane.
+plot(xy_dat)
 %now imagine there is some fluorescence data corresponding to the same
 %cells
 fluor = rand(100,1);
 % plot the data in the xy plane but color code the data by the fluorescnece
 % values. Hint: see the scatter command
+scatter(xy_dat(:,1),xy_dat(:,2),[], fluor)
